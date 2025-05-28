b569e66bb05058cda51b00aad53d37f3911bfef5
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-05-28 13:07:48 +0530

    Edit & Update The React APP With Links & My Photo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from "./VP.jpeg"
 import './App.css';
 
 function App() {
@@ -7,7 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          <b>By VP</b>
+          <br />
+          <a href="https://github.com/Paramesh-Kumar-2004" target="_b">Github</a>
+          &#160;&#160;&#160;
+          <a href="https://www.linkedin.com/in/paramesh-kumar-s-047632249/" target="_b">LinkedIn</a>
+          <br /><br />
           This repository is created to test the deployment of a React application using GitHub Pages (gh-pages). It demonstrates how to set up a React project for deployment, configure the gh-pages package, and host the build output on GitHub Pages for public access.
+          <br />
+          Thank You
         </p>
       </header>
     </div>
